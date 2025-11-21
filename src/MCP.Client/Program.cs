6f42127c274965716c163e8a9260598802b53d2a
Augusto <porti.sk8@gmail.com>
2025-11-20 22:42:06 -0300

    First commit AgentFramework

---
+++ src/MCP.Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using OpenAI;
+
+
+string? openAiKey = Environment.GetEnvironmentVariable("OpenAI-KEY");
+string model = "gpt-5-nano";
+if (string.IsNullOrWhiteSpace(openAiKey))
+{
+    Console.WriteLine("Error: La variable de entorno 'OpenAI-KEY' no está configurada.");
+    return;
+}
+
+AIAgent agent = new OpenAIClient(openAiKey)
+    .GetChatClient(model)
+    .CreateAIAgent(instructions: "You are good at telling jokes.", name: "Joker");
+
+
+Console.WriteLine(await agent.RunAsync("Tell me a joke about a pirate."));
